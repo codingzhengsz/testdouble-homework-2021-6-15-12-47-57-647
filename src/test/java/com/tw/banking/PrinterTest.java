8bbb0d87f2c7488e5a5f3663e50539820e0a8738
sutton <codingzhengsz@163.com>
2021-06-24 23:09:11 +0800

    [Martin] test-case: should_return_statementLine_given_transaction_and_running_balance

--- src/test/java/com/tw/banking/PrinterTest.java
+++ src/test/java/com/tw/banking/PrinterTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class PrinterTest {
@@ -21,6 +22,20 @@ public class PrinterTest {
 
         // then
         verify(console, times(1)).printLine(Printer.STATEMENT_HEADER);
+    }
+
+    @Test
+    void should_return_statementLine_given_transaction_and_running_balance() {
+        // given
+        Transaction transaction = new Transaction("2021/06/24", 10);
+        int runningBalance = 10;
+
+        Printer printer = new Printer(new Console());
+        // when
+        String statementLine = printer.statementLine(transaction, runningBalance);
+
+        // then
+        assertEquals("2021/06/24 | 10 | 10", statementLine);
     }
 
 }
